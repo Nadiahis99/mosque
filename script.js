c2e5fad7def78bd3f865a845585806d07a5a4f81
Nadia Hisham <125770853+Nadiahis99@users.noreply.github.com>
2026-02-10 15:15:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Slider
+// Ayat rotation
+const ayat = [
+  "﴿ إِنَّمَا يَعْمُرُ مَسَاجِدَ اللَّهِ مَنْ آمَنَ بِاللَّهِ ﴾",
+  "﴿ وَمَا تُقَدِّمُوا لِأَنْفُسِكُمْ مِنْ خَيْرٍ تَجِدُوهُ عِندَ اللَّهِ ﴾",
+  "﴿ مَثَلُ الَّذِينَ يُنفِقُونَ أَمْوَالَهُمْ فِي سَبِيلِ اللَّهِ ﴾"
+];
+
+let i = 0;
+const ayahEl = document.getElementById("ayah");
+ayahEl.innerText = ayat[0];
+
+setInterval(() => {
+  i = (i + 1) % ayat.length;
+  ayahEl.innerText = ayat[i];
+}, 6000);
+
+// Slider placeholder
+function prevSlide(){/* تكملة لاحقًا */}
+function nextSlide(){/* تكملة لاحقًا */}
+
 let slides = document.querySelectorAll('.slide');
 let current = 0;
 function nextSlide() {
